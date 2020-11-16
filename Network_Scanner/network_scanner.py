bb136c361e608eddb2f490da0f7248a4aa9d94ff
ariyonaty <ari.h.yonaty@gmail.com>
2020-11-15 23:10:26 -0800

    More work on network scanner.

--- Network_Scanner/network_scanner.py
+++ Network_Scanner/network_scanner.py
@@ -1,17 +1,38 @@
+import optparse
 from scapy.all import srp
 from scapy.layers.l2 import ARP, Ether
 
 # iface = "wlx9cefd5fd63ca"
 
 
+def menu():
+    print("------------------------------------------")
+    print("|            NETWORK SCANNER             |")
+    print("------------------------------------------")
+
+
 def scan(ip):
     arp_request = ARP(pdst=ip)
     broadcast = Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_request_broadcast = broadcast / arp_request
-    answered = srp(arp_request_broadcast, timeout=1)[0]
-    print(answered.summary())
+    answered = srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+
+    client_list = []
 
     for resp in answered:
-        print (resp)
+        client_dict = {"ip": resp[1].psrc, "mac": resp[1].hwsrc}
+        client_list.append(client_dict)
 
-scan("10.0.2.2/24")
+    return client_list
+
+def display(client_list):
+    print("IP\t\t\tMAC ADDR")
+    print("------------------------------------------")
+    for client in client_list:
+        print(f'{client["ip"]}\t\t{client["mac"]}')
+
+if __name__ == "__main__":
+    menu()
+    results = scan("10.0.2.2/24")
+    display(results)
+
