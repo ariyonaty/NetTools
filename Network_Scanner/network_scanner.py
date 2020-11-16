4c7cd8fda7ac53b672764149c6a8448af025da26
ariyonaty <ari.h.yonaty@gmail.com>
2020-11-15 16:08:21 -0800

    cont. work on network scanner.

--- Network_Scanner/network_scanner.py
+++ Network_Scanner/network_scanner.py
@@ -1,13 +1,17 @@
 from scapy.all import srp
 from scapy.layers.l2 import ARP, Ether
 
-iface = "wlx9cefd5fd63ca"
+# iface = "wlx9cefd5fd63ca"
+
 
 def scan(ip):
     arp_request = ARP(pdst=ip)
     broadcast = Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_request_broadcast = broadcast / arp_request
-    answered, unanswered = srp(arp_request_broadcast, iface=iface)
+    answered = srp(arp_request_broadcast, timeout=1)[0]
     print(answered.summary())
 
-scan("192.168.88.1")
+    for resp in answered:
+        print (resp)
+
+scan("10.0.2.2/24")
