07868173f5c7eb5c308a91b15ef6655dfcf14a3d
ariyonaty <ari.h.yonaty@gmail.com>
2020-12-10 23:49:32 -0800

    TODO: figure out how to extract  RWA packets :

--- Packet_Sniffer/packet_sniffer.py
+++ Packet_Sniffer/packet_sniffer.py
@@ -1,12 +1,16 @@
 import scapy.all as scapy
 from scapy.layers import http
 
+
 def sniff(interface):
     scapy.sniff(iface=interface, store=False, prn=process_sniffed_packet)
 
-def process_sniffed_packet(packet:scapy.packet):
+
+def process_sniffed_packet(packet: scapy.packet):
+    print(packet.show())
     if (packet.haslayer(http.HTTPRequest)):
-        print(packet)
+        if (packet.haslayer(scapy.Raw)):
+            print(packet[scapy.Raw].load)
 
 if __name__ == "__main__":
     sniff('eth0')
