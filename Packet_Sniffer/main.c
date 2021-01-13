6fe7db865d78e5b54b9be58d3cfb5bb3b60e70fd
ariyonaty <ari.h.yonaty@gmail.com>
2021-01-13 01:38:00 -0800

    Adding more to the network tools

---
+++ Packet_Sniffer/main.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <string.h>
+#include <net/ethernet.h>
+#include <netinet/ip.h>
+#include <netinet/if_ether.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+
+#define PACKET_BUFFER_SIZE 65536
+
+int main(int argc, char const *argv[])
+{
+    int error_code = 0;
+    ssize_t data_size;
+    uint8_t packet_buffer[PACKET_BUFFER_SIZE];
+
+    if (argc != 2)
+    {
+        fprintf(stderr, "[-] Usage: %s [interface]\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    const char *interface_name = argv[1];
+
+    int raw_sock = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
+    if (raw_sock == -1)
+    {
+        perror("socket");
+        return EXIT_FAILURE;
+    }
+
+    if (setsockopt(raw_sock, SOL_SOCKET, SO_BINDTODEVICE, interface_name, strlen(interface_name)) == -1)
+    {
+        perror("setsockopt");
+        return EXIT_FAILURE;
+    }
+
+    for (;;)
+    {
+        data_size = recvfrom(raw_sock, packet_buffer, PACKET_BUFFER_SIZE, 0, NULL, NULL);
+        if (data_size == -1)
+        {
+            perror("recvfrom");
+            return EXIT_FAILURE;
+        }
+        // packet recvd at this point
+        printf("Recieved: %lu bytes\n", data_size);
+    }
+
+    return EXIT_SUCCESS;
+}
+
--- Port_Scanner/port_scanner.py
+++ Port_Scanner/port_scanner.py
@@ -5,13 +5,26 @@
 import socket
 import threading
 
+results = {}
+
 def worker(ip, port):
-    pass
+    s = socket.socket(socket.AF_INET, )
+    s.settimeout(0.5)
+    if s.connect_ex((ip, port)) == 0:
+        if port == 80:
+            s.send(b'GET / HTTP/1.0\r\n\r\n')
+        results[port] = s.recv(1024).decode()
+
 
 def scan(ip):
     for port in range(1000):
         t = threading.Thread(target=worker, args=(ip, port))
         t.start()
 
+
 if __name__ == "__main__":
     ip = input('Enter target IP > ')
+    scan(ip)
+    for port, data in results.items():
+        print(f'Port {port}\t\t{data}')
+        
